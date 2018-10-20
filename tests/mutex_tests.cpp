3cb97f953e3d4831ff3c5512934551287b9bd4f9
TwITe <dm.blink19@gmail.com>
2018-10-21 01:28:51 +0300

    Add Mutex tests

---
+++ tests/mutex_tests.cpp
@@ -0,0 +1,45 @@
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+#include <iostream>
+#include <vector>
+#include <thread>
+#include "../Mutex.h"
+
+using namespace std;
+
+Mutex lock;
+int cnt = 0;
+
+const int THREADS_NUM = 100;
+const int OP_PER_THREAD = 100;
+
+void reset_count() {
+    cnt = 0;
+}
+
+void thread_run(int thread_num) {
+    for (int i = 0; i < OP_PER_THREAD; i++) {
+        lock.acquire();
+        cnt++;
+        cout << "[Thread " << thread_num << "] " << cnt << endl;
+        lock.release();
+    }
+}
+
+TEST_CASE("Concurrent count increment") {
+    for (int current_try = 0; current_try < 10; current_try++) {
+        vector<thread> threads;
+        threads.reserve(THREADS_NUM);
+        for (int i = 0; i < THREADS_NUM; i++) {
+            threads.emplace_back(thread(thread_run, i));
+        }
+
+        for (int i = 0; i < THREADS_NUM; i++) {
+            threads[i].join();
+        }
+
+        REQUIRE(cnt == THREADS_NUM * OP_PER_THREAD);
+        reset_count();
+    }
+}
