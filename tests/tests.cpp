e13d196ba59ed56726979f9b306d8bc3d48aa3be
TwITe <dm.blink19@gmail.com>
2018-10-28 13:12:19 +0300

    Update tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -16,16 +16,14 @@ Mutex lock;
 Spinlock lock;
 #endif
 
-int cnt = 0;
-
 int THREADS_NUM;
 int OP_PER_THREAD;
 
-void do_increment(int thread_num) {
+void do_increment(int thread_num, int* cnt) {
     for (int i = 0; i < OP_PER_THREAD; i++) {
         lock.acquire();
-        cnt++;
-        cout << "[Thread " << thread_num << "] " << cnt << endl;
+        (*cnt)++;
+        cout << "[Thread " << thread_num << "] " << *cnt << endl;
         lock.release();
     }
 }
@@ -34,11 +32,13 @@ TEST_CASE("Concurrent count increment") {
     THREADS_NUM = 100;
     OP_PER_THREAD = 100;
 
+    int cnt = 0;
+
     for (int current_try = 0; current_try < 10; current_try++) {
         vector<thread> threads;
         threads.reserve(THREADS_NUM);
-        for (int i = 0; i < THREADS_NUM; i++) {
-            threads.emplace_back(thread(do_increment, i));
+        for (int current_thread = 0; current_thread < THREADS_NUM; current_thread++) {
+            threads.emplace_back(thread(do_increment, current_thread, &cnt));
         }
 
         for (int i = 0; i < THREADS_NUM; i++) {
@@ -50,82 +50,57 @@ TEST_CASE("Concurrent count increment") {
     }
 }
 
-void try_acquire(bool* can_lock) {
-    *can_lock = lock.try_lock();
+void do_acquire(volatile atomic<bool>* locked) {
+    *locked = lock.try_lock();
+    while (!(*locked)) {cout << "Can not acquire\n";};
 }
 
-TEST_CASE("Test Mutual exclusion") {
-    THREADS_NUM = 2;
-
-    for (int current_try = 0; current_try < 10; current_try++) {
-        bool can_lock[THREADS_NUM];
-        vector<thread> threads;
-        threads.reserve(THREADS_NUM);
-        for (int i = 0; i < THREADS_NUM; i++) {
-            threads.emplace_back(thread(try_acquire, &can_lock[i]));
-        }
-
-        for (int i = 0; i < THREADS_NUM; i++) {
-            threads[i].join();
-        }
-        lock.release();
-
-        if (can_lock[0]) {
-            REQUIRE(!can_lock[1]); // if first thread acquired a lock so other thread can not acquire lock
-        } else {
-            REQUIRE(can_lock[1]); // if first threads can not acquire a lock so the other thread acquired it
-        }
-    }
-}
-
-void do_acquire(atomic<bool>* can_lock) {
-    while (!(*can_lock = lock.try_lock()));
-}
-
-void find_n_prime_number(int n, atomic<bool>* can_lock) {
+void find_n_prime_number(int n, volatile atomic<bool>* locked) {
     lock.acquire();
-    *can_lock = true;
+    *locked = true;
 
-    int current_prime_number_cnt = 0;
+    int prime_numbers_cnt = 0;
+    int current_num = 2;
 
-    for (int i = 2; i > 0; i++) {
-        current_prime_number_cnt++;
-        int upper_bound = static_cast<int>(sqrt(i));
-        for (int j = 2; j <= upper_bound; j++) {
-            if (i % j == 0) {
-                current_prime_number_cnt--;
+    while (true) {
+        prime_numbers_cnt++;
+        for (int j = 2; j <= static_cast<int>(sqrt(current_num)); j++) {
+            if (current_num % j == 0) {
+                prime_numbers_cnt--;
                 break;
             }
         }
 
-        if (current_prime_number_cnt == n) { // found n-th prime number
+        if (prime_numbers_cnt == n) { // found n-th prime number
             break;
         }
     }
 
     lock.release();
-    *can_lock = false;
+    *locked = false;
 }
 
-TEST_CASE("Test Mutual Exclusion 2") {
+TEST_CASE("Unable acquire a lock while other thread already acquired it") {
     THREADS_NUM = 2;
 
     for (int current_try = 0; current_try < 10; current_try++) {
-        atomic<bool> can_lock[THREADS_NUM];
+        volatile atomic<bool> locked[THREADS_NUM]; // monitoring which thread acquired a lock
+
         vector<thread> threads;
         threads.reserve(THREADS_NUM);
-
-        threads.emplace_back(thread(find_n_prime_number, 100, &can_lock[0]));
-        threads.emplace_back(thread(do_acquire, &can_lock[1]));
+        threads.emplace_back(thread(find_n_prime_number, 20, &locked[0]));
+        while (!locked[0]); // wait until first thread acquire a lock to put other thread to work
+        threads.emplace_back(thread(do_acquire, &locked[1]));
 
         for (int i = 0; i < THREADS_NUM; i++) {
-            threads[i].join();
+            threads[i].detach();
         }
-        lock.release();
 
-        while (can_lock[0]) {
-            REQUIRE(!can_lock[1]); // thread 1 can not acquire a lock while thread 0 already acquired it
+        while (locked[0]) {
+            REQUIRE(!locked[1]); // thread 1 can not acquire a lock while thread 0 already acquired it
         }
-        REQUIRE(can_lock[1]); // thread 1 can acquire a lock if thread 0 released it
+        REQUIRE(locked[1]); // thread 1 can acquire a lock since thread 0 released it
+
+        lock.release();
     }
 }
